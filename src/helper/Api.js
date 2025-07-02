a2ff5ace70de547076f3a42022c5c1dfb29f6d29
khurramshahzad-12 <ks6771455@gmail.com>
2025-07-02 20:22:58 +0400

    update-slider-api

--- src/helper/Api.js
+++ src/helper/Api.js
@@ -57,7 +57,7 @@ const getallsubcategories = async (rbody = {proId:''}) => {return await fetchDat
 const getallsubsubcategories = async(rbody = {proId:''}) => {return await fetchData(`/api/get_all_sub_sub_categories`,'POST',rbody);}
 const addsubcategory = async (rbody) => {return await fetchData(`/api/add_sub_category`, 'POST', rbody);}
 const addsubsubcategory = async (rbody) => {return await fetchData(`/api/add_sub_sub_category`, 'POST', rbody);}
-const addhomeslider = async (formData) => {return await fetchData(`${frontEndURL}/api/add_home_slider`, 'POST', formData);}
+const addhomeslider = async (formData) => {return await fetchData(`/api/add_home_slider`, 'POST', formData);}
 const addsupplierslider = async (formData) => {return await fetchData(`/api/add_supplier_slider`, 'POST', formData);}
 const registerpromotionemail = async (rbody) => {return await fetchData(`${frontEndURL}/api/register_promo_email`, 'POST', rbody);}
 const upsertterms = async (rbody) => {return await fetchData(`${frontEndURL}/api/upsert_terms_conditions`, 'POST', rbody);}
