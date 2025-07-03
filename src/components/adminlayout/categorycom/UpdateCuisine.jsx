6c58f643f3acdb7d22437ee9e263b6de7a34a829
khurramshahzad-12 <ks6771455@gmail.com>
2025-07-03 08:30:13 +0400

    update-css-design

--- src/components/adminlayout/categorycom/UpdateCuisine.jsx
+++ src/components/adminlayout/categorycom/UpdateCuisine.jsx
@@ -40,14 +40,14 @@ const UpdateCuisine = ({ proId, setDialogueOpen, cuisine, type, setCheckChanges 
     }
     let formData = type === 'cuisine' ? cuisines : spCategories;
     return (
-        <section className='relative'>
+        <section className='relative z-20'>
             <IoMdClose className='text-lg absolute right-4 top-4 text-secondary cursor-pointer' onClick={handleClosebtn} />
-            <div className='w-48 bg-gray-200 ring-2 ring-gray-300 rounded-sm p-2 shadow-sm'>
+            <div className='w-56 bg-gray-100 ring-2 ring-gray-300 rounded-sm p-2 shadow-sm'>
                 <div className='ring-gray-300 ring-1 p-2 rounded-sm shadow-md bg-white'>
                     <h6 className='sm:text-sm text-center font-medium text-secondary pb-2 pt-2'>{type === 'cuisine'?'Choose Cuisine' : 'Choose Feature Category'}</h6>
                     {
                         formData && formData.map((csne, index) => (
-                            <div className='flex py-1 hover:bg-green-100 items-center' key={index}>
+                            <div className='flex py-1 m-2 hover:bg-green-100 items-center' key={index}>
                                 <input type="checkbox"
                                     value={csne.name}
                                     checked={allCheck && allCheck.includes(csne.name)}
