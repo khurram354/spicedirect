624e997a50392a5a6e04b490e455319ed393fe7b
khurramshahzad-12 <ks6771455@gmail.com>
2025-07-07 14:52:27 +0400

    update-cred

--- src/helper/senEmail.js
+++ src/helper/senEmail.js
@@ -2,24 +2,18 @@ import { createTransport } from "nodemailer";
 
 const sendEmail = async (name,from, mobileNo, message) => {
     const transporter = createTransport({
-        service: "gmail",
-        host: "smtp.gmail.com",
+        host: "smtp.office365.com",
         port: 587,
         auth: {
             user: process.env.EMAIL_USER,
             pass: process.env.EMAIL_PASSWORD
         }
-        // host: 'smtp.office365.com',
-        // port: 587,
-        // auth: {
-        //            user: process.env.EMAIL_USER,
-        //     pass: process.env.EMAIL_PASSWORD
-        // }
     });
     const resp = await transporter.sendMail({
+        from:`"Spice Direct Wholesale Website" <${process.env.EMAIL_USER}>`,
         to: process.env.EMAIL_USER,
         replyTo: from,
-        subject: "Spice Direct Wholesale",
+        subject: "Spice Direct Wholesale Contact Form",
         html: `
         <h4>Name: ${name} </h4>
         <h4>Mobile Number: ${mobileNo} </h4>
