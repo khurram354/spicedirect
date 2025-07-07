b2483be563b6ccb579cbd9d6e89fe4b371ab7d0c
khurramshahzad-12 <ks6771455@gmail.com>
2025-07-07 22:14:21 +0400

    footer-css

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -20,7 +20,7 @@ const Footer = () => {
                 className="w-full h-full object-contain"
               />
             </div>
-            <div className="mt-8 grid grid-cols-2 sm:gap-8 2xl:gap-x-24 sm:gap-y-10 lg:mt-0 lg:grid-cols-5 lg:gap-y-8 lg:justify-content-center">
+            <div className="mt-8 grid grid-cols-2 sm:gap-10 md:gap-x-20 xl:gap-x-24 2xl:gap-x-28 sm:gap-y-10 lg:mt-0 lg:grid-cols-5 lg:gap-y-8">
               {/* <div className="col-span-2">
                 <div>
                   <h2 className="text-2xl font-bold text-gray-900">Get the latest news!</h2>
