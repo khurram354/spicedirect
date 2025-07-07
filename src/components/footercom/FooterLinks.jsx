772d17225a9b54b2897fb004624ab3ba78709361
khurramshahzad-12 <ks6771455@gmail.com>
2025-07-07 21:39:03 +0400

    uddate_footercss

--- src/components/footercom/FooterLinks.jsx
+++ src/components/footercom/FooterLinks.jsx
@@ -10,7 +10,7 @@ const FooterLinks = () => {
                         <p className="font-medium text-gray-900">{fmenu.name}</p>
                         {
                             fmenu && fmenu.subitems.map((item, index) => (
-                                <ul className="mt-6 space-y-4 text-sm" key={index}>
+                                <ul className="mt-4 space-y-4 text-sm" key={index}>
                                     <li>
                                         <Link href={`${item.link}`} className="text-medium transition hover:text-secondary text-gray-600 font-medium"> {item.subItemName} </Link>
                                     </li>
