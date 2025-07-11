15e6acf5b2b4e43a6842672bc9b2ccecf7094cae
khurramshahzad-12 <ks6771455@gmail.com>
2025-07-11 18:01:57 +0400

    added-basic-seo

--- src/app/layout.js
+++ src/app/layout.js
@@ -5,7 +5,37 @@ import StoreProvider from "./StoreProvider";
 
 export const metadata = {
   title: "Spice Direct WholeSale",
-  description: "Best wholesale food provider in glosgow, UK",
+  description: "Best wholesale food supplier in glasgow, UK. Competitive Prices, Good Quality",
+  keywords: ['wholesale food', 'spices', 'grocery','cheese', 'meat', 'UK supplier'],
+  metadataBase: new URL('https://www.spicedirectwholesale.co.uk'),
+  alternates: {
+    canonical: "/",
+  },
+  openGraph:{
+    title: "Spice Direct Wholesale",
+    description:"Best wholesale food supplier in glasgow, UK. Competitive Prices, Good Quality",
+    url:"https://www.spicedirectwholesale.co.uk",
+    siteName:"Spice Direct Wholesale",
+    images:[{url:"/spice_direct_wholesale.jpg", width:1200, height:630}],
+    locale:"en_UK",
+    type:"website"
+  },
+  twitter:{
+    card:"summary_large_image",
+    title:"Spice Direct Wholesale",
+    description:"Best wholesale food supplier in glasgow, UK. Competitive Prices, Good Quality",
+    images:["/spice_direct_wholesale.jpg"]
+  },
+  robots:{
+    index:true,
+    follow:true,
+    nocache:false,
+    googleBot:{index:true,follow:true,noimageindex:false},
+  },
+  icons:{
+    icon:"/favicon.ico",
+    apple:"/apple-touch-icon.png"
+  }
 };
 
 export default function RootLayout({ children }) {
