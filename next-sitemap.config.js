8427e03079c4c2f56e7cefb33e8f2c4074f07848
khurramshahzad-12 <ks6771455@gmail.com>
2025-07-14 00:28:58 +0400

    added-sitemap

---
+++ next-sitemap.config.js
@@ -0,0 +1,35 @@
+module.exports = {
+  siteUrl: 'https://spicedirectwholesale.co.uk',
+  generateRobotsTxt: true,
+  robotsTxtOptions: {
+    policies: [
+      { 
+        userAgent: '*', 
+        allow: '/',
+        disallow: ['/admin', '/dashboard'] 
+      }
+    ],
+    additionalSitemaps: [
+      'https://spicedirectwholesale.co.uk/sitemap.xml',
+    ],
+  },
+  exclude: [
+    '/server-sitemap.xml', 
+    '/admin/*',
+    '/api/*'
+  ],
+  transform: async (config, path) => {
+    let priority = 0.8;
+    if (path === '/') priority = 1.0;
+    if (path.includes('/product')) priority = 0.9;
+    
+    return {
+      loc: path,
+      changefreq: 'daily',
+      priority,
+      lastmod: config.autoLastmod ? new Date().toISOString() : undefined,
+    };
+  },
+  sitemapSize: 7000,
+  generateIndexSitemap: true
+};
