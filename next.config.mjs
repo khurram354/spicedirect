92a2d67299ed76a234262509f5b6bbda2faf645c
khurramshahzad-12 <ks6771455@gmail.com>
2025-07-28 20:57:48 +0400

    config

--- next.config.mjs
+++ next.config.mjs
@@ -1,10 +1,18 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-    images:{
-        remotePatterns:[
+    webpack(config, { isServer }) {
+        if (isServer) {
+            config.externals.push({
+                canvas: 'commonjs canvas',
+            });
+        }
+        return config;
+    },
+    images: {
+        remotePatterns: [
             {
-                protocol:"https",
-                hostname:"spicedirectwholesale.s3.eu-west-2.amazonaws.com"
+                protocol: "https",
+                hostname: "spicedirectwholesale.s3.eu-west-2.amazonaws.com"
             }
         ]
     }
