22f519f54f323d9a8679c6a6f4744738f932f73d
khurramshahzad-12 <ks6771455@gmail.com>
2025-07-28 20:34:00 +0400

    move-to-client-side

--- src/components/promotions/PdfWrapper.jsx
+++ src/components/promotions/PdfWrapper.jsx
@@ -1,6 +1,7 @@
 "use client";
 
-import PdfViewer from "./PdfViewer";
+import dynamic from "next/dynamic";
+const PdfViewer = dynamic(() => import("./PdfViewer"), { ssr: false });
 
 const PdfWrapper = () => {
     return <PdfViewer file="/data/spiceDirectPromotions.pdf" />;
