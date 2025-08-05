69c4e56280566f63d234080a99619b1e8eb9a106
khurramshahzad-12 <ks6771455@gmail.com>
2025-08-05 14:34:14 +0400

    remove-inventory-api

---
+++ src/app/api/(products)/remove_inventory/route.js
@@ -0,0 +1,23 @@
+import dbConnect from "@/lib/db";
+import InventoryProduct from "@/models/inventorySchema";
+import { handleError } from "@/utils/errorHandler";
+import { handleSuccess } from "@/utils/handleSuccess";
+import mongoose from "mongoose";
+
+export async function DELETE(request) {
+    try {
+        const { id } = await request.json();
+        if (!id) { return handleError(null, "missing required fields") }
+        if (!mongoose.Types.ObjectId.isValid(id)) {
+            return handleError(null, "Invalid product ID");
+        }
+        await dbConnect();
+        const resp = await InventoryProduct.findByIdAndDelete(id);
+        if (resp) {
+            return handleSuccess(null, "product", "product deleted successfully")
+        } else {
+            return handleSuccess(null, "products", "no matching product found to delete")
+        }
+
+    } catch (error) { return handleError(error) }
+}
