72b915c873afc4765ee6cd9e167d04fd8ce59f78
khurramshahzad-12 <ks6771455@gmail.com>
2025-08-19 18:49:25 +0400

    change-notification-time

--- src/lib/crobjob.js
+++ src/lib/crobjob.js
@@ -1,8 +1,8 @@
 import cron from "node-cron";
 
 export function startCronJobs(){
-    // cron.schedule("0 15 * * *",async()=>{
-    cron.schedule("*/1 * * * *",async()=>{
+    cron.schedule("0 15 * * *",async()=>{
+    // cron.schedule("*/1 * * * *",async()=>{
         try {
             await fetch(`${process.env.NEXT_PUBLIC_BASE_URL}/api/send_notification`)
         } catch (error) {
