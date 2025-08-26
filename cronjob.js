c5ac51f27134b301d1457fab4ef45af0207d56c5
khurramshahzad-12 <ks6771455@gmail.com>
2025-08-26 22:18:11 +0400

    update-time

--- cronjob.js
+++ cronjob.js
@@ -1,8 +1,8 @@
 const cron = require("node-cron");
 
 function startCronJobs(){
-    // cron.schedule("0 15 * * *",async()=>{
-    cron.schedule("*/1 * * * *",async()=>{
+    cron.schedule("0 15 * * *",async()=>{
+    // cron.schedule("*/1 * * * *",async()=>{
         try {
             await fetch(`https://www.spicedirectwholesale.co.uk/api/send_notification`)
         } catch (error) {
